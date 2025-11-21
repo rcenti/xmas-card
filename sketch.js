905f8643a51f25a6cfb6d4c75a78842dcfe256e3
rcenti <roberta_centi@outlook.com>
2025-11-21 01:10:01 +0000

    continue

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let background;
+let maiBig;
+let maiSmall;
+let font;
+let star;
+
+function preload() {
+    font = loadFont('assets/CoromorantGaramond-Italic-VariableFont_wght.ttf');
+    background = loadImage('assets/pixilart-drawing.png');
+    maiBig = loadImage('assets/portrait.png');
+    maiSmall = loadImage('assets/pixil-frame-0 (1).png');
+    star = loadImage('assets/pixil-frame-0.png');
+}
+
+function setup() {
