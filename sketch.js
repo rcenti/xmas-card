6ecf3b5f655faba602616bea08aba0140c4a5c0c
rcenti <roberta_centi@outlook.com>
2025-11-25 16:38:04 +0000

    continue

--- sketch.js
+++ sketch.js
@@ -3,9 +3,10 @@ let maiBig;
 let maiSmall;
 let font;
 let star;
+let clicked = false;
 
 function preload() {
-    font = loadFont('assets/CoromorantGaramond-Italic-VariableFont_wght.ttf');
+    font = loadFont('assets/CrimsonText-Regular.ttf');
     backgroundImage = loadImage('assets/pixilart-drawing.png');
     maiBig = loadImage('assets/portrait.png');
     maiSmall = loadImage('assets/pixil-frame-0 (1).png');
@@ -16,14 +17,56 @@ function setup() {
     imageMode(CENTER);
     createCanvas(600,600);
     textFont(font);
+    textAlign(CENTER, CENTER);
+    
 
 }
 
 function draw() {
-    background(backgroundImage);
+    background(255);
+    rectMode(CENTER);
+    rect(width /2 , 580, 600, // fix this code );
+    welcomeScreen();
+
+    fill(242, 230, 220);
+    
+    //image(backgroundImage, width/2, height/2, 600, 600);
 
 }
 
-function mouseClicked() {
-    if (mouseClicked() || 
+function welcomeScreen() {
+    textSize(32);
+    fill(0);
+    
+    if (!clicked) {
+        text("Click to start", width / 2, height / 2);
+    } else {
+        text("it was the night before christmas.", width / 2, height / 2);
+    }
+}
+
+
+
+
+
+
+function rectangleClicked() {
+    const rw = 500;
+    const rh = 100;
+    const rx = width / 2;
+    const ry = height - rh;
+  
+// Check if mouse is inside the rectangle
+    if (
+        mouseX >= rx - rw/2 && mouseX <= rx + rw/2 &&
+        mouseY >= ry - rh/2 && mouseY <= ry + rh/2
+    ) {
+        clicked = true;   // change state
+    }
+}
+ 
+
+function mousePressed() {
+    rectangleClicked();
+
 }
